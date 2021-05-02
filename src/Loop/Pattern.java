7929f55d20c3031b93d75ff22d6e21ba3e5e04df
DELL <himanshumtr12@gmail.com>
2021-05-02 10:05:32 +0530

    Patter

---
+++ src/Loop/Pattern.java
@@ -0,0 +1,24 @@
+package Loop;
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String args[]){
+            Scanner obj=new Scanner(System.in);
+            int n=obj.nextInt();
+            int number=1;
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n-i;j++){
+                    System.out.print(" ");
+                }
+//                for(int j=1;j<=i;j++){
+//                    System.out.print("* ");
+//                }
+                for(int j=1;j<=i;j++){
+                    System.out.print(number+" ");
+                    number++;
+                }
+                    System.out.println();
+            }
+    }
+
+}
